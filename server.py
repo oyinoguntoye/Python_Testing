a91d6ca5dce16beb0765d3a002c8e2ef18dbd423
oyinoguntoye <oyinoguntoye@hotmail.com>
2020-07-27 21:17:06 +0100

    Update server.py

--- server.py
+++ server.py
@@ -47,7 +47,7 @@ def book(competition,club):
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions,clubs=clubs)
 
 
 @app.route('/purchasePlaces',methods=['POST'])
@@ -57,7 +57,7 @@ def purchasePlaces():
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions,clubs=clubs)
 
 
 # TODO: Add route for points display
